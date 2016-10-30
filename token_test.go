c9a468019dfda3826ba6d135bc776cf02598e7d6
Marcel Voigt <mv@noch.so>
2016-10-30 11:04:43 +0100

    Test Token.String()

---
+++ token_test.go
@@ -0,0 +1,20 @@
+package cml
+
+import "testing"
+
+func TestToken_String(t *testing.T) {
+	tok := Token{Line: 1, Type: TokenValue, Str: "abc"}
+	act := tok.String()
+	exp := `1 TokenValue "abc"`
+	if exp != act {
+		t.Fatalf("expected %#v; got %#v", exp, act)
+	}
+
+	// Invalid/unknown TokenType
+	tok.Type = -1
+	exp = `1 TokenType(-1) "abc"`
+	act = tok.String()
+	if exp != act {
+		t.Fatalf("expected %#v; got %#v", exp, act)
+	}
+}
