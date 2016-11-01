3022da063fdbf713145d7f564f45a38f7945d84b
Marcel Voigt <mv@noch.so>
2016-11-01 13:41:04 +0100

    Run sub test per golden file

--- parser_test.go
+++ parser_test.go
@@ -7,22 +7,22 @@ import (
 )
 
 func TestParse_Ok(t *testing.T) {
-	for tc := range golden.Dir(t, "parser/ok") {
+	golden.TestDir(t, "parser/ok", func(tc golden.Case) {
 		r := tc.In.Reader()
 		doc, err := Parse(r)
 		r.Close()
 		if err != nil {
-			t.Errorf("%s: %s", tc.In.Path, err)
+			tc.T.Error(err)
 		}
 		if *update {
 			tc.Out.Update([]byte(doc.String()))
 		}
 		tc.Diff(doc.String())
-	}
+	})
 }
 
 func TestParse_Error(t *testing.T) {
-	for tc := range golden.Dir(t, "parser/error") {
+	golden.TestDir(t, "parser/error", func(tc golden.Case) {
 		r := tc.In.Reader()
 		_, err := Parse(r)
 		r.Close()
@@ -30,5 +30,5 @@ func TestParse_Error(t *testing.T) {
 			tc.Out.Update([]byte(err.Error()))
 		}
 		tc.Diff(err.Error())
-	}
+	})
 }
