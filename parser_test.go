7b8a7ede8129f2a9dcfa3da3e46c3fbcc145e8dd
Marcel Voigt <mv@noch.so>
2016-11-01 11:08:50 +0100

    Test Parse for errors

--- parser_test.go
+++ parser_test.go
@@ -20,3 +20,15 @@ func TestParse_Ok(t *testing.T) {
 		tc.Diff(doc.String())
 	}
 }
+
+func TestParse_Error(t *testing.T) {
+	for tc := range golden.Dir(t, "parser/error") {
+		r := tc.In.Reader()
+		_, err := Parse(r)
+		r.Close()
+		if *update {
+			tc.Out.Update([]byte(err.Error()))
+		}
+		tc.Diff(err.Error())
+	}
+}
