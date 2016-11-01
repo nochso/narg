d552e71816c05b0e039752921b49b8820e151d95
Marcel Voigt <mv@noch.so>
2016-11-01 14:56:15 +0100

    Parse nested items

--- parser.go
+++ parser.go
@@ -1,6 +1,7 @@
 package narg
 
 import (
+	"errors"
 	"fmt"
 	"io"
 )
@@ -14,6 +15,7 @@ type Parser struct {
 	l *Lexer
 }
 
+// Parse all items.
 func (p *Parser) Parse() (Doc, error) {
 	doc := []Item{}
 	item, err := p.parse()
@@ -24,16 +26,40 @@ func (p *Parser) Parse() (Doc, error) {
 	if err == io.EOF {
 		err = nil
 	}
+	if err == errEos || err == errSos {
+		t := p.l.Token
+		err = fmt.Errorf("line %d, column %d: expected quoted or unquoted value at beginning of item, got %s %#v", t.Line, t.Col, t.Type, t.Str)
+	}
 	return doc, err
 }
 
+// parse exactly one Item
 func (p *Parser) parse() (i Item, err error) {
 	i, err = p.parseName(i)
 	if err != nil {
 		return
 	}
 	i, err = p.parseArgs(i)
+	if err == errSos {
+		i, err = p.parseChildren(i)
+		if err == errEos { // ok, it ends with a '}'
+			err = nil
+		}
+		return
+	}
 	return
+}
+
+var errEos = errors.New("end of child section")
+var errSos = errors.New("start of child section")
+
+func (p *Parser) parseChildren(i Item) (Item, error) {
+	child, err := p.parse()
+	for err == nil {
+		i.Children = append(i.Children, child)
+		child, err = p.parse()
+	}
+	return i, err
 }
 
 func (p *Parser) parseName(i Item) (Item, error) {
@@ -43,6 +69,12 @@ func (p *Parser) parseName(i Item) (Item, error) {
 	}
 	if t.Error() != nil {
 		return i, t.Error()
+	}
+	if t.Type == TokenBraceClose {
+		return i, errEos
+	}
+	if t.Type == TokenBraceOpen {
+		return i, errSos
 	}
 	if t.Type != TokenQuotedValue && t.Type != TokenUnquotedValue {
 		err := fmt.Errorf("line %d, column %d: expected quoted or unquoted value at beginning of item, got %s %#v", t.Line, t.Col, t.Type, t.Str)
@@ -62,8 +94,11 @@ func (p *Parser) parseArgs(i Item) (Item, error) {
 		if t.Error() != nil {
 			return i, t.Error()
 		}
-		if t.Type == TokenBraceOpen || t.Type == TokenBraceClose {
-			return i, fmt.Errorf("nested items not yet implemented")
+		if t.Type == TokenBraceOpen {
+			return i, errSos
+		}
+		if t.Type == TokenBraceClose {
+			return i, errEos
 		}
 		if t.Type != TokenQuotedValue && t.Type != TokenUnquotedValue {
 			err := fmt.Errorf("line %d, column %d: expected quoted or unquoted value as argument no. %d, got %s %#v", t.Line, t.Col, len(i.Args)+1, t.Type, t.Str)
