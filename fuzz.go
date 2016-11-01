3878dc9bf5a5a893afd5c6abb9ba815d37d0dd18
Marcel Voigt <mv@noch.so>
2016-11-01 17:04:47 +0100

    Add fuzz func and basic corpus for go-fuzz

---
+++ fuzz.go
@@ -0,0 +1,19 @@
+package narg
+
+import "bytes"
+
+// Fuzz is exposed only for internal use with go-fuzz: https://github.com/dvyukov/go-fuzz
+//
+// Corpus resides in test-fixtures/fuzz/corpus
+//
+//  go get github.com/dvyukov/go-fuzz/go-fuzz
+//  go get github.com/dvyukov/go-fuzz/go-fuzz-build
+//  go-fuzz-build github.com/nochso/narg
+//  go-fuzz -bin narg-fuzz.zip -workdir test-fixtures/fuzz
+func Fuzz(data []byte) int {
+	_, err := Parse(bytes.NewReader(data))
+	if err != nil {
+		return 0
+	}
+	return 1
+}
