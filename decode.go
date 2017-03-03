1b5a2b3cba017413d5f922b1001f592e16e7ce45
Marcel Voigt <mv@noch.so>
2017-03-03 22:54:54 +0100

    Add Decode() to turn narg into structs

---
+++ decode.go
@@ -0,0 +1,128 @@
+package narg
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func Decode(r io.Reader, v interface{}) error {
+	items, err := Parse(r)
+	if err != nil {
+		return err
+	}
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr {
+		return fmt.Errorf("decode of non-pointer %s", rv.Type())
+	}
+	if rv.IsNil() {
+		return fmt.Errorf("decode of nil %s", rv.Type())
+	}
+	return decStruct(rv.Elem(), &Item{Children: items})
+}
+
+func decStruct(v reflect.Value, item *Item) error {
+	for i := 0; i < v.NumField(); i++ {
+		f := v.Field(i)
+		key := strings.ToLower(v.Type().Field(i).Name)
+		tag, ok := v.Type().Field(i).Tag.Lookup("narg")
+		if ok {
+			// append positional arg to named children
+			pos, err := strconv.ParseInt(tag, 10, 64)
+			if err != nil {
+				return err
+			}
+			if pos < int64(len(item.Args)) {
+				if len(item.Children.Filter(key)) == 0 {
+					item.Children = append(item.Children, Item{Name: key, Args: []string{item.Args[pos]}})
+				}
+			}
+		}
+		for _, itm := range item.Children.Filter(key) {
+			err := dec(f, &itm)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func dec(f reflect.Value, item *Item) error {
+	if item == nil {
+		return nil
+	}
+	var err error
+	switch f.Kind() {
+	case reflect.String:
+		f.SetString(item.Args[0])
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		err = decInt(f, item)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		err = decUint(f, item)
+	case reflect.Float32, reflect.Float64:
+		err = decFloat(f, item)
+	case reflect.Bool:
+		return decBool(f, item)
+	case reflect.Slice:
+		err = decSlice(f, item)
+	case reflect.Struct:
+		err = decStruct(f, item)
+	default:
+		err = fmt.Errorf("can not decode unknown type %s", f.Kind())
+	}
+	return err
+}
+
+func decBool(f reflect.Value, item *Item) error {
+	b, err := strconv.ParseBool(item.Args[0])
+	if err == nil {
+		f.SetBool(b)
+	}
+	return err
+}
+
+func decSlice(f reflect.Value, item *Item) error {
+	if f.Type().Elem().Kind() == reflect.Struct {
+		ptr := reflect.New(f.Type().Elem()).Interface()
+		s := reflect.ValueOf(ptr).Elem()
+		err := decStruct(s, item)
+		f.Set(reflect.Append(f, s))
+		return err
+	}
+	f.Set(reflect.MakeSlice(f.Type(), len(item.Args), len(item.Args)))
+	f.SetLen(len(item.Args))
+	for n, arg := range item.Args {
+		err := dec(f.Index(n), &Item{Args: []string{arg}})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func decFloat(f reflect.Value, item *Item) error {
+	x, err := strconv.ParseFloat(item.Args[0], f.Type().Bits())
+	if err == nil {
+		f.SetFloat(x)
+	}
+	return err
+}
+
+func decInt(f reflect.Value, item *Item) error {
+	i, err := strconv.ParseInt(item.Args[0], 0, f.Type().Bits())
+	if err == nil {
+		f.SetInt(i)
+	}
+	return err
+}
+
+func decUint(f reflect.Value, item *Item) error {
+	i, err := strconv.ParseUint(item.Args[0], 0, f.Type().Bits())
+	if err == nil {
+		f.SetUint(i)
+	}
+	return err
+}
---
+++ decode_test.go
@@ -0,0 +1,72 @@
+package narg
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/kylelemons/godebug/pretty"
+)
+
+type testConf struct {
+	Name   string
+	Port   int
+	Debug  bool
+	Float  float32
+	Hosts  []string
+	Ports  []int
+	PortsU []uint8
+	Admin  testUser
+	Users  []testUser
+}
+
+type testUser struct {
+	ID   int    `narg:"0"`
+	Name string `narg:"1"`
+}
+
+func TestDecode(t *testing.T) {
+	act := &testConf{}
+	exp := &testConf{
+		Name:   "foo",
+		Port:   80,
+		Debug:  true,
+		Float:  3.14,
+		Hosts:  []string{"a", "bee", "cee e"},
+		Ports:  []int{1024, 1025},
+		PortsU: []uint8{0xff, 0xff},
+		Admin: testUser{
+			ID:   1,
+			Name: `Phil "Tandy" Miller`,
+		},
+		Users: []testUser{
+			{2, "Carol Pilbasian"},
+			{4, "Todd"},
+		},
+	}
+	in := `
+name foo
+port 80
+debug 1
+float 3.14
+hosts a bee "cee e"
+ports 1024 1025
+portsu 0xff 255
+admin {
+	id 1
+	name "Phil \"Tandy\" Miller"
+}
+users {
+	id 2
+	name "Carol Pilbasian"
+}
+users 4 Todd
+`
+	err := Decode(strings.NewReader(in), act)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(exp, act) {
+		t.Fatal(pretty.Compare(act, exp))
+	}
+}
