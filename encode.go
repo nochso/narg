e4c700c7b39121a2ccf2df2c4190a22f63bd160d
Marcel Voigt <mv@noch.so>
2017-03-11 20:41:16 +0100

    Add and test Encode

---
+++ encode.go
@@ -0,0 +1,65 @@
+package narg
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"strings"
+)
+
+func Encode(w io.Writer, v interface{}) error {
+	rv := reflect.ValueOf(v)
+	for rv.Kind() == reflect.Ptr {
+		rv = reflect.Indirect(rv)
+	}
+	if rv.Kind() != reflect.Struct {
+		return fmt.Errorf("unsupported kind: %s", rv.Kind())
+	}
+	itm, err := encodeStruct("", rv)
+	if err != nil {
+		return err
+	}
+	for i, item := range itm.Children {
+		if i > 0 {
+			w.Write([]byte{'\n'})
+		}
+		item.writeString(w, 0)
+	}
+	return nil
+}
+
+func encodeStruct(name string, v reflect.Value) (Item, error) {
+	itm := Item{}
+	itm.Name = name
+	var err error
+	for i := 0; i < v.NumField(); i++ {
+		ft := v.Type().Field(i)
+		var child Item
+		if ft.Type.Kind() == reflect.Struct {
+			child, err = encodeStruct(strings.ToLower(ft.Name), v.Field(i))
+			if err != nil {
+				return itm, err
+			}
+		} else if ft.Type.Kind() == reflect.Slice {
+			if ft.Type.Elem().Kind() == reflect.Struct {
+				for x := 0; x < v.Field(i).Len(); x++ {
+					foo, err := encodeStruct(strings.ToLower(ft.Name), v.Field(i).Index(x))
+					if err != nil {
+						return itm, err
+					}
+					itm.Children = append(itm.Children, foo)
+				}
+				continue
+			} else {
+				for x := 0; x < v.Field(i).Len(); x++ {
+					child.Args = append(child.Args, fmt.Sprintf("%v", v.Field(i).Index(x)))
+				}
+			}
+		} else {
+			child.Args = append(child.Args, fmt.Sprintf("%v", v.Field(i)))
+		}
+		child.Name = strings.ToLower(ft.Name)
+		itm.Children = append(itm.Children, child)
+	}
+	return itm, err
+}
---
+++ encode_test.go
@@ -0,0 +1,56 @@
+package narg
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/kylelemons/godebug/diff"
+)
+
+func TestEncode(t *testing.T) {
+	v := &testConf{
+		Name:   "foo",
+		Port:   80,
+		Debug:  true,
+		Float:  3.14,
+		Hosts:  []string{"a", "bee", "cee e"},
+		Ports:  []int{1024, 1025},
+		PortsU: []uint8{0xff, 0xff},
+		Admin: testUser{
+			ID:   1,
+			Name: `Phil "Tandy" Miller`,
+		},
+		Users: []testUser{
+			{2, "Carol Pilbasian"},
+			{4, "Todd"},
+		},
+	}
+	exp := `name foo
+port 80
+debug true
+float 3.14
+hosts a bee "cee e"
+ports 1024 1025
+portsu 255 255
+admin {
+	id 1
+	name "Phil \"Tandy\" Miller"
+}
+users {
+	id 2
+	name "Carol Pilbasian"
+}
+users {
+	id 4
+	name Todd
+}`
+	buf := &bytes.Buffer{}
+	err := Encode(buf, v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	act := buf.String()
+	if exp != act {
+		t.Error(diff.Diff(exp, act))
+	}
+}
